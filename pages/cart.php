29368129508e300b9bbdc21fe6ef3c23f580d7cb
Zyxary <kelpselp@gmail.com>
2025-12-08 19:58:56 +0800

    Cart: A cart ui

--- pages/cart.php
+++ pages/cart.php
@@ -1,3 +1,21 @@
+<?php 
+    session_start();
+    require_once __DIR__ . "/../config/database.php";
+    require_once __DIR__ . "/../Classes/CartClass.php";
+    require_once __DIR__ . "/../includes/functions.php";
+
+    if(isset($_POST['checkout']))
+    {
+        if (!headers_sent()) 
+        {
+            redirectToPage('checkout.php');
+        } 
+    }
+
+    $cart = new Cart();
+    $cartItems = $cart->getCartItems($_SESSION['user_id']);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,14 +27,44 @@
     <div style="text-align: center;">
         <h1> THIS IS YOUR SHOPPING CART </h1>
 
-        <ul>
-            <li>LAPTOP</li>
-            <li>PHONE</li>
-            <li>KEYBOARD</li>
-            <li>MOUSE</li>
-        </ul>
-        <h3>TOTAL: WOW</h3>
-    </div>
+        <div>
+            <table border="5" cellpadding="20">
+                <tr>
+                    <th>Product name</th>
+                    <th>price</th>
+                    <th>stock</th>
+                    <th>RAM</th>
+                    <th>ROM</th>
+                </tr>
+
+                <?php 
+                        
+                    if($cartItems && count($cartItems) > 0)
+                    { 
+                        foreach($cartItems as $item)
+                        { ?>
+                            <tr>
+                                <td><?= $item['name'] ?></td>
+                                <td><?= $item['price'] ?></td>
+                                <td><?= $item['stock'] ?></td>
+                                <td><?= $item['RAM'] ?></td>
+                                <td><?= $item['ROM'] ?></td>
+                            </tr>
+
+                        <?php }  
+                    }
+                    else
+                    {
+                        echo "<tr><td colspan='7'>Cart is Empty :(</td></tr>";
+                    }
+            
+                ?>
+                 <tr>
+                    <td>Total:</td>
+                </tr>
+
+            </table>
+        </div>
 
     <div>
         <form action="cart.php" method="post">
@@ -25,15 +73,3 @@
     </div>
 </body>
 </html>
-
-<?php 
-    require_once __DIR__ . "/../includes/functions.php";
-
-    if(isset($_POST['checkout']))
-    {
-        if (!headers_sent()) 
-        {
-            redirectToPage('checkout.php');
-        } 
-    }
-?>
