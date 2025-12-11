13bb71b60abf509b96f1cc2f7f95fb69db850028
Zyxary <kelpselp@gmail.com>
2025-12-11 11:56:57 +0800

    HTML and CSS for checkout and cart page

--- pages/pc.php
+++ pages/pc.php
@@ -101,6 +101,7 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manzanas Computers - Pro Performance</title>
     <link rel="stylesheet" href="../pages/pcStyles.css">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"> <!-- placeholder laanay ini since waray paman kita icons   -->
     <style>
         .buy-form {
             margin-top: 20px;
